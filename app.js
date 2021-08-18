eb4ff11d87d324c328b3aafdc7882dc8e72e91ca
Aditya Padia <aditya.padia0911@gmail.com>
2021-08-18 09:15:25 +0800

    Cleared misc files and improved comments

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-//Routes /  = Takes to different web pages like www.instagram.com/pixeunt
+// Route to direct to the homepage of the application
 app.get("/", function(req, res){
 	res.render("homepage.ejs");
 });
 
 //404 Error route = *
 //Error route must always be at the end otherwise no routes will exist
-
 app.get("*", function(req, res){
 	res.send("Error! That route doesn't exist. You are lost");
 });
